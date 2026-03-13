90e2b2db7210d70a162131973dedd7602ce62033
Tifelabs <boluwatifeolusegun018@gmail.com>
2026-03-12 21:00:26 -0600

    fix color duplicate issue

--- src/ui/color.hpp
+++ src/ui/color.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 namespace Color {
-  const char* RESET   = "\033[0m";
-  const char* BOLD    = "\033[1m";
-  const char* DIM     = "\033[2m";
-  const char* RED     = "\033[31m";
-  const char* GREEN   = "\033[32m";
-  const char* YELLOW  = "\033[33m";
-  const char* BLUE    = "\033[34m";
-  const char* MAGENTA = "\033[35m";
-  const char* CYAN    = "\033[36m";
-  const char* WHITE   = "\033[37m";
-  const char* BG_BLACK = "\033[40m";
+  inline const char* RESET    = "\033[0m";
+  inline const char* BOLD     = "\033[1m";
+  inline const char* DIM      = "\033[2m";
+  inline const char* RED      = "\033[31m";
+  inline const char* GREEN    = "\033[32m";
+  inline const char* YELLOW   = "\033[33m";
+  inline const char* BLUE     = "\033[34m";
+  inline const char* MAGENTA  = "\033[35m";
+  inline const char* CYAN     = "\033[36m";
+  inline const char* WHITE    = "\033[37m";
+  inline const char* BG_BLACK = "\033[40m";
 }
